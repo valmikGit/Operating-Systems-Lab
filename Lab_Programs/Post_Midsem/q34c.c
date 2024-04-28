8e84f1c05782896b13a0991a0416f5fd82310502
valmikGit <quantumcomputingiiitb@gmail.com>
2024-04-29 00:15:18 +0530

    modified 34c.c

--- Lab_Programs/Post_Midsem/q34c.c
+++ Lab_Programs/Post_Midsem/q34c.c
@@ -1,23 +1,26 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<unistd.h>
-#include<fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
 
-int main(){   //using fcntl
-    int fd[2];
-    pipe(fd);
-    int x=fork();
-    if(x==0){
-        close(1);
-        close(fd[0]);
-        fcntl(fd[1],F_DUPFD);
-        execlp("ls","ls","-l",NULL);
-    }
-    else{
-        close(0);
-        close(fd[1]);
-        fcntl(fd[0],F_DUPFD);
-        execlp("wc","wc",NULL);
+int main() {
+    int pipe_fd[2];
+    pipe(pipe_fd);
+
+    if (fork() == 0) {
+        // Child process
+        close(pipe_fd[0]); // Close read end of the pipe
+        fcntl(pipe_fd[1], F_SETFD, FD_CLOEXEC); // Close pipe_fd[1] on exec
+        dup2(pipe_fd[1], STDOUT_FILENO); // Redirect stdout to write end of pipe
+        close(pipe_fd[1]); // Close pipe_fd[1]
+        execlp("ls", "ls", "-l", NULL); // Execute ls -l
+    } else {
+        // Parent process
+        close(pipe_fd[1]); // Close write end of the pipe
+        dup2(pipe_fd[0], STDIN_FILENO); // Redirect stdin to read end of pipe
+        close(pipe_fd[0]); // Close pipe_fd[0]
+        execlp("wc", "wc", NULL); // Execute wc
     }
 
+    return 0;
 }
