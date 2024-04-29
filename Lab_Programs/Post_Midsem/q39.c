427b42a85586f96c4abccd2b816e9af6c3ea9c7f
valmikGit <quantumcomputingiiitb@gmail.com>
2024-04-29 13:47:12 +0530

    modified q39.c

--- Lab_Programs/Post_Midsem/q39.c
+++ Lab_Programs/Post_Midsem/q39.c
@@ -1,55 +1,61 @@
 #include <stdio.h>
-#include <unistd.h>
-#include <sys/stat.h>
-#include <sys/types.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <fcntl.h>
 #include <sys/time.h>
+#include <sys/stat.h>
 
-// Use 22_write.c as writer.
-//doubt
-int main()
-{
-    fd_set rfds;
-    struct timeval tv;
-    int retval;
-
+int main() {
     int fd;
+    char *fifo = "/tmp/myfifo";
 
-    printf("10 second timer will start when writer is available.\n");
+    // Create the FIFO (named pipe)
+    mkfifo(fifo, 0666);
 
-    fd = open("myFifo", O_RDONLY, 0666);
-    /* Watch fd of myFifo to see when it is ready for reading. */
-    printf("Starting 10 second timer.\n");
+    // Open the FIFO for reading
+    fd = open(fifo, O_RDONLY | O_NONBLOCK); // Open in non-blocking mode
 
-    FD_ZERO(&rfds);
-    FD_SET(fd, &rfds);
-
-    /* Wait up to ten seconds. */
-
-    tv.tv_sec = 10;
-    tv.tv_usec = 0;
-
-    // Waiting
-    // printf("Waiting...\n");
-    retval = select(fd + 1, &rfds, NULL, NULL, &tv);
-    // printf("Waiting Over\n");
-
-    if (retval == -1){
-        perror("select()");
-    }
-    else if (retval){
-        printf("Data is available now.\n");
-        char buf[100];
-        read(fd, buf, sizeof(buf));
-        printf("Message from writer: %s", buf);
-    }
-    else{
-        printf("No data within ten seconds.\n");
+    if (fd == -1) {
+        perror("open");
+        exit(EXIT_FAILURE);
     }
 
+    fd_set read_fds;
+    struct timeval timeout;
+
+    // Set the timeout to 10 seconds
+    timeout.tv_sec = 10;
+    timeout.tv_usec = 0;
+
+    // Initialize the file descriptor set
+    FD_ZERO(&read_fds);
+    FD_SET(fd, &read_fds);
+
+    // Wait for data to be available in the FIFO or timeout after 10 seconds
+    int result = select(fd + 1, &read_fds, NULL, NULL, &timeout);
+
+    if (result == -1) {
+        perror("select");
+        exit(EXIT_FAILURE);
+    } else if (result == 0) {
+        printf("No data available in FIFO after 10 seconds.\n");
+    } else {
+        printf("Data available in FIFO.\n");
+
+        // Read data from the FIFO
+        char buf[256];
+        ssize_t bytes_read = read(fd, buf, sizeof(buf));
+
+        if (bytes_read == -1) {
+            perror("read");
+            exit(EXIT_FAILURE);
+        }
+
+        printf("Received data from FIFO: %s\n", buf);
+    }
+
+    // Close the FIFO
     close(fd);
-    exit(EXIT_SUCCESS);
 
-    return (0);
+    return 0;
 }
